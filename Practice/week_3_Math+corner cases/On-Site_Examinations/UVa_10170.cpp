daf7c2f517055fbd3ace118b23be884eaf5e3cd7
Iris.Lin <909918464qq@gmail.com>
2026-04-13 22:36:00 +0800

    訂正UVa_10170

--- Practice/week_3_Math+corner cases/On-Site_Examinations/UVa_10170.cpp
+++ Practice/week_3_Math+corner cases/On-Site_Examinations/UVa_10170.cpp
@@ -1,21 +1,17 @@
+//✅
 #include <iostream>
 using namespace std;
 
 int main(){
-	for(int l=0 ; l<100000 ; l++){
-		int s,d,ans;
-		int n=0;
-		cin>>s>>d;
-		int count=0;
-		
-		for(int i=s ; i<=d ; i++){
-			n+=i;
-			if(n>=d){
-				ans=i;
-				break;
-			}	
+	long long s,d;
+	while (cin>>s>>d){
+		long long sum=0,ans;
+		while(sum<d){
+			ans=s;
+			sum+=s;
+			s++;//忘記要加一
 		}
-		cout<<ans<<endl;	
+		cout<<ans<<endl;
 	}
 	return 0;
 }
