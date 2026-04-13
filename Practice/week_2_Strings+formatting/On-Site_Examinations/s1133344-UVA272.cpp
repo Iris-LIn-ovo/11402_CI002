e6d3152015bdee28383f1b9311dad69fc063fad4
Iris.Lin <909918464qq@gmail.com>
2026-04-13 20:55:07 +0800

    訂正UVa_272

--- Practice/week_2_Strings+formatting/On-Site_Examinations/s1133344-UVA272.cpp
+++ Practice/week_2_Strings+formatting/On-Site_Examinations/s1133344-UVA272.cpp
@@ -3,21 +3,25 @@
 using namespace std;
 
 int main(){
+
 	string a;
-	int count=1;
-	while(cin>>a){
-		if(a == "\"" && count%2 == 1){
-			cout<<"``";
-			count++;
+	bool count=0;
+	while(getline(cin, a)){
+		for(int i=0; i<a.size(); i++){
+			if(a[i] == '"' && count == 0){//用單引號包雙引號，代表文字雙引號
+				cout<<"``";
+				count=1;
+			}
+			else if(a[i] == '"' && count == 1){
+				cout<<"''";
+				count=0;
+			}
+			else{
+				cout<<a[i];
+			}
 		}
-		else if(a == "\"" && count%2 == 0){
-			cout<<"''";
-			count++;
-		}
-		else{
-			cout<<a<<" ";
-		}	
-			
+		cout<<endl;//一定要加！！！不然過不了
+		
 	}
 	return 0;
 }
--- Practice/week_7_Binary search+requency counting/On-Site_Examinations/UVa_11063.cpp
+++ Practice/week_7_Binary search+requency counting/On-Site_Examinations/UVa_11063.cpp
@@ -8,11 +8,14 @@ int main(){
 	int count=1;
 	
 	while(cin>>n){
+		bool isb2=1;
 		vector<int> num;
 		for(int i=0; i<n; i++){
 			int x;
 			cin>>x;
 			num.push_back(x);
+			if (x<=0)isb2=0;//小於等於0為非，原本只寫小於
+			else if(i>0 && x<num[i-1])isb2=0;//非嚴格遞增也為非
 		}
 		
 		vector<int> add;	
@@ -24,23 +27,21 @@ int main(){
 		}
 		sort(add.begin(), add.end());
 		
-		bool isb2=1;
+		
 		for(int i=0; i<add.size()-1; i++){
 			if(add[i]==add[i+1]){
 				isb2=0;
 			}
 		}
-		
-		if(n<2 || n>100){
-			cout<<"Case #"<<count<<": It is not a B2-Sequence."<<endl;
-		}
-		else if(n>=2 && n<=100 && isb2==0){
+		//if(n<2 || n>100)題目會保證範圍，不用自己判斷
+		if(isb2==0){
 			cout<<"Case #"<<count<<": It is not a B2-Sequence."<<endl;
 		}
 		else{
-			cout<<"Case #"<<count<<": Is a B2-Sequence."<<endl;
+			cout<<"Case #"<<count<<": It is a B2-Sequence."<<endl;
 		}
 		count++;
+		cout<<endl;//一定要換行
 	}
 	
 	return 0;
